b6c8355d81a5f2aee9d8a084b15749b4db89148e
y-bsndxng <yoshida37350526@outlook.com>
2026-03-17 21:33:41 +0900

    [add] first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+        <h1>Hello World</h1>
+        <p>React + Vite + GitHub Pages</p>
+      </main>
+    </>
+  )
+}
+
+export default App
